c64af6afaf696dd869810713722aeb6536e1fab8
jesusemanuel87 <jesusemanuelgarcia@gmail.com>
2025-09-24 18:54:38 -0300

    Initial commit

---
+++ app/src/test/java/com/example/tp03_android_cargar_listar_productos_salir/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.tp03_android_cargar_listar_productos_salir;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
